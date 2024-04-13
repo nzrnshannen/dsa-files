74043550db8eca54012544d94d6565ac8ed892aa
Shannen Nazareno <nzrnshannen@gmail.com>
2024-04-13 22:18:20 +0800

    added adt bst ll codes

--- Pre-finals/Trees/Pre-final Runthrough/ADT_BST-LL-getEven_getOdd.c
+++ Pre-finals/Trees/Pre-final Runthrough/ADT_BST-LL-getEven_getOdd.c
@@ -9,21 +9,24 @@ typedef struct node{
     struct node *RC;
 }Cell, *BST;
 
-void initTree(BST * Tree);
+void initTree(BST *Tree);
 void populate(BST *Tree);
 bool isMember(BST Tree, int elem);
 
+void insertMem(BST *Tree, int elem);
+void populate(BST *Tree);
+BST getEven(BST Tree, BST evenTree);
+BST getOdd(BST Tree, BST oddTree);
 void insertMem(BST *Tree, int elem);
 void deleteMem(BST *Tree, int elem);
 int deleteMin(BST *Tree);
 int deleteMax(BST *Tree);
 
+void display(BST Tree);
+void levelOrder(BST Tree);
 void preorder(BST Tree);
 void postorder(BST Tree);
 void inorder(BST Tree);
-
-void levelOrder(BST Tree);
-void display(BST Tree);
 
 void initTree(BST *Tree)
 {
@@ -58,16 +61,16 @@ void insertMem(BST *Tree, int elem)
 
 int deleteMin(BST *Tree)
 {
-    BST *trav, temp;
     int num;
+    BST *trav, temp;
 
     for(trav=Tree; *trav!=NULL && (*trav)->LC!=NULL; trav=&(*trav)->LC){}
 
-    if(*trav!=NULL) //if tree is not empty
+    if(*trav!=NULL)
     {
         temp = *trav;
         num = temp->elem;
-        *trav = temp->RC; //it's like adopting the child of the leftmost node to be deleted :)
+        *trav = temp->RC;
         free(temp);
     }
 
@@ -76,16 +79,16 @@ int deleteMin(BST *Tree)
 
 int deleteMax(BST *Tree)
 {
-    BST *trav, temp;
     int num;
+    BST *trav, temp;
 
     for(trav=Tree; *trav!=NULL && (*trav)->RC!=NULL; trav=&(*trav)->RC){}
 
-    if(*trav!=NULL) //if tree is not empty
+    if(*trav!=NULL)
     {
         temp = *trav;
         num = temp->elem;
-        *trav = temp->LC; //it's like adopting the child of the rightmost node to be deleted :)
+        *trav = temp->LC;
         free(temp);
     }
 
@@ -96,29 +99,65 @@ void deleteMem(BST *Tree, int elem)
 {
     BST *trav;
 
-    //searching for elem if it exists in the BST
     for(trav=Tree; *trav!=NULL && (*trav)->elem!=elem; )
     {
         trav = (elem < (*trav)->elem) ? &(*trav)->LC : &(*trav)->RC;
     }
 
-    if(*trav!=NULL) //if the node to be deleted is found
+    if(*trav!=NULL)
     {
         BST temp;
         temp = *trav;
-
-        if(temp->LC==NULL || temp->RC==NULL)
+        
+        if(temp->LC==NULL && temp->RC==NULL)
         {
             *trav = (temp->LC!=NULL) ? temp->LC : temp->RC;
             free(temp);
         }
         else
         {
-            //using the successor
-
-            //using temp (PN) only since we're gonna be modifying the data part only
-            temp->elem = deleteMin(&temp->RC);
+            //using predecessor this time
+            temp->elem = deleteMax(&temp->LC);
         }
+    }
+}
+
+void display(BST Tree)
+{
+    if(Tree!=NULL)
+    {
+        printf("\n===========================\nTREE: ");
+        levelOrder(Tree);
+        printf("\n\n");
+    }
+    else
+    {
+        printf("\n\t\tEmpty Tree!\n\n");
+    }
+}
+
+void levelOrder(BST Tree)
+{
+    BST tempQueue[SIZE], current;
+    int front = 1, rear = 0;
+
+    if(Tree!=NULL)
+    {
+        for(tempQueue[++rear] = Tree; front!=(rear+1)%SIZE; )
+        {
+            current = tempQueue[front];
+            front = (front+1)%SIZE;
+
+            if(current!=NULL)
+            {
+                printf("%d ", current->elem);
+
+                rear = (rear+1)%SIZE;
+                tempQueue[rear] = current->LC;
+                rear = (rear+1)%SIZE;
+                tempQueue[rear] = current->RC;
+            }
+        }  
     }
 }
 
@@ -152,74 +191,62 @@ void inorder(BST Tree)
     }
 }
 
-void levelOrder(BST Tree)
+BST getOdd(BST Tree, BST oddTree)
 {
-    BST tempQueue[SIZE], current;
-    int front=1, rear=0;
-
     if(Tree!=NULL)
     {
-        for(tempQueue[++rear]=Tree; front!=(rear+1)%SIZE; )
+        if(Tree->elem%2==1)
         {
-            current = tempQueue[front];
-            front = (front+1)%SIZE;
-
-            if(current!=NULL)
-            {
-                printf("%d ", current->elem);
-
-                rear = (rear+1)%SIZE;
-                tempQueue[rear] = current->LC;
-                rear= (rear+1)%SIZE;
-                tempQueue[rear] = current->RC;
-            }
+            insertMem(&oddTree, Tree->elem);
         }
+
+        oddTree = getOdd(Tree->LC, oddTree);
+        oddTree = getOdd(Tree->RC, oddTree);
     }
+    
+    return oddTree;
 }
 
-void display(BST Tree)
+BST getEven(BST Tree, BST evenTree)
 {
     if(Tree!=NULL)
     {
-        printf("\n===================================\nTree: ");
-        levelOrder(Tree);
-        printf("\n\n");
+        if(Tree->elem%2==0)
+        {
+            insertMem(&evenTree, Tree->elem);
+        }
+        evenTree = getEven(Tree->LC, evenTree);
+        evenTree = getEven(Tree->RC, evenTree);
     }
-    else
-    {
-        printf("\n\n\tEmpty Tree!\n\n");
-    }
+    
+    return evenTree;
 }
-
 void populate(BST *Tree)
 {
     insertMem(Tree, 10);
+    insertMem(Tree, 30);
     insertMem(Tree, 5);
-    insertMem(Tree, 2);
-    insertMem(Tree, 9);
-    insertMem(Tree, 20);
+    insertMem(Tree, 19);
+    insertMem(Tree, 8);
+    insertMem(Tree, 70);
 }
 
 int main()
 {
     BST myTree;
-
     initTree(&myTree);
+    display(myTree);
+
     populate(&myTree);
-
     display(myTree);
 
-    printf("Inorder: ");
-    inorder(myTree);
+    BST evenTree = getEven(myTree, NULL);
+    printf("EVEN");
+    display(evenTree);
 
-    printf("\nPreoder: ");
-    preorder(myTree);
-
-    printf("\nPostorder: ");
-    postorder(myTree);
-
-    deleteMem(&myTree, 9);
-    display(myTree);
+    BST oddTree = getOdd(myTree, NULL);
+    printf("ODD");
+    display(oddTree);
 
     return 0;
 }
